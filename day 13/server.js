3ed2a617396a8e72733054b08970509160e6cea0
naik03shashank <naik03shashank@users.noreply.github.com>
2026-06-05 21:45:23 +0530

    Add Day 13 files

---
+++ day 13/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app=express();
+app.use(express.json());
+
+
+app.use(express.static('public'));
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+})
